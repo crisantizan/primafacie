ec01ec05ece5968c0c5a8f8e36be3290173cf272
Cristian Santiz <crisantizan@gmail.com>
2020-12-10 22:56:34 -0500

    chore: export named

--- src/main.js
+++ src/main.js
@@ -6,9 +6,6 @@ import Button from './components/Button.vue';
 import Input from './components/Input.vue';
 
 // export components for this library
-export default {
-  Button,
-  Input
-};
+export { Button, Input };
 
 createApp(App).mount('#app');
