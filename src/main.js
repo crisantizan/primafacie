11facaa121e68827f7f5746221b3c7390e0b0585
Cristian Santiz <crisantizan@gmail.com>
2020-12-10 22:25:28 -0500

    feat: components and config

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
-import { createApp } from "vue";
-import App from "./App.vue";
+import { createApp } from 'vue';
+import App from './App.vue';
 
-createApp(App).mount("#app");
+// import components here
+import Button from './components/Button.vue';
+import Input from './components/Input.vue';
+
+// export components for this library
+export default {
+  Button,
+  Input
+};
+
+createApp(App).mount('#app');
