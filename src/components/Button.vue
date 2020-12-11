7f52ca0b463415a46284771ceb06cfe8a93cdd2f
Cristian Santiz <crisantizan@gmail.com>
2020-12-10 22:20:51 -0500

    first commit

---
+++ src/components/Button.vue
@@ -0,0 +1,15 @@
+<template>
+  <button @click="onClick">Button</button>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  methods: {
+    onClick() {
+      console.log('Click');
+    }
+  }
+});
+</script>
---
+++ src/components/Input.vue
@@ -0,0 +1,14 @@
+<template>
+  <input type="text" v-model="input" />
+  <h2>{{ input }}</h2>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  data: () => ({
+    input: 'from Vue3'
+  })
+});
+</script>
